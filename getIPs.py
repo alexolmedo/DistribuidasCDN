087aab590b1726846f160c4a526cc9972f99273b
Mayra <mayra.rosero@epn.edu.ec>
2018-02-24 07:49:42 -0500

    Obtener nombre de interfaz de red wifi

--- getIPs.py
+++ getIPs.py
@@ -3,9 +3,10 @@ import fcntl
 import struct
 from netaddr import IPAddress, IPNetwork
 import signal
+import commands
 
 #Obtener IP local
-iface = "wlp3s0"
+iface = commands.getoutput('ls /sys/class/net | grep wl')
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 ipLocal = str(s.getsockname()[0])
@@ -13,7 +14,7 @@ print ipLocal
 s.close()
 
 #Obtener mascara de subnet
-subnetMask = str (IPAddress(socket.inet_ntoa(fcntl.ioctl(socket.socket(socket.AF_INET, socket.SOCK_DGRAM), 35099, struct.pack('256s', iface))[20:24])).netmask_bits())
+subnetMask = str (IPAddress(socket.inet_ntoa(fcntl.ioctl(socket.socket(socket.AF_INET, socket.SOCK_DGRAM), 35099, struct.pack('256s', str(iface)))[20:24])).netmask_bits())
 print subnetMask
 
 #Obtener lista de hosts de la subnet actual
