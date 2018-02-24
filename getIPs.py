0d6c604205347b00d4555098a91e8522ff4b217d
alexander <olmedo2408@gmail.com>
2018-02-24 11:16:03 -0500

    Obtener IP y máscara de subred

---
+++ getIPs.py
@@ -0,0 +1,9 @@
+import socket
+import fcntl
+import struct
+iface = "wlp3s0"
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+print(s.getsockname()[0])
+s.close()
+print socket.inet_ntoa(fcntl.ioctl(socket.socket(socket.AF_INET, socket.SOCK_DGRAM), 35099, struct.pack('256s', iface))[20:24])
