06b440b78f03e433a0dfdc867549fde48df252c3
alexander <olmedo2408@gmail.com>
2018-02-24 11:40:46 -0500

    Obtener lista de  hosts de la subnet actual

--- getIPs.py
+++ getIPs.py
@@ -1,9 +1,21 @@
 import socket
 import fcntl
 import struct
+from netaddr import IPAddress, IPNetwork
+
+#Obtener IP local
 iface = "wlp3s0"
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
-print(s.getsockname()[0])
+ipLocal = str(s.getsockname()[0])
+print ipLocal
 s.close()
-print socket.inet_ntoa(fcntl.ioctl(socket.socket(socket.AF_INET, socket.SOCK_DGRAM), 35099, struct.pack('256s', iface))[20:24])
+
+#Obtener mascara de subnet
+subnetMask = str (IPAddress(socket.inet_ntoa(fcntl.ioctl(socket.socket(socket.AF_INET, socket.SOCK_DGRAM), 35099, struct.pack('256s', iface))[20:24])).netmask_bits())
+print subnetMask
+
+#Obtener lista de hosts de la subnet actual
+for ip in IPNetwork(ipLocal + "/" + subnetMask):
+    print '%s' % ip
+
